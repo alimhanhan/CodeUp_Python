6342406c4cb214f034365de254e5677a86fc9064
ALim <102565567+alimhanhan@users.noreply.github.com>
2022-09-01 20:22:15 +0900

    Add files via upload

---
+++ CodeUp_6007, 6008, 6009.py
@@ -0,0 +1,17 @@
+# 6007
+
+print('\"C:\Download\\\'hello\'.py\"')   # ', " 뿐만 아니라 역슬래시(\)를 출력할 때도 역슬래시를 앞에 붙여야 함
+
+
+
+
+# 6008
+
+print('print("Hello\\nWorld")')
+
+
+
+# 6009
+
+m=input()
+print(m)
