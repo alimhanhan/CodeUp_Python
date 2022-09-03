d251eb04f8b16598b8312e401f21fcf622e490a9
ALim <102565567+alimhanhan@users.noreply.github.com>
2022-09-03 14:37:11 +0900

    Add files via upload

---
+++ CodeUp_6013, 6014, 6015.py
@@ -0,0 +1,27 @@
+# 6013
+
+a= input()
+b =input()
+
+print(b)
+print(a)
+
+
+
+
+# 6014
+
+a = float(input())
+
+print(a)
+print(a)
+print(a)
+
+
+
+# 6015
+
+a, b = map(int, input().split())
+
+print(a)
+print(b)
