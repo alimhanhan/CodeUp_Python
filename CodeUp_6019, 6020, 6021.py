e15775ab2e01da50a52c94a5f4c192819aecba7f
ALim <102565567+alimhanhan@users.noreply.github.com>
2022-09-05 18:31:07 +0900

    Add files via upload

---
+++ CodeUp_6019, 6020, 6021.py
@@ -0,0 +1,25 @@
+# 6019
+
+a, b, c = input().split('.')
+
+print(c,b,a, sep='-')
+
+
+
+# 6020
+
+a,b = input().split('-')
+
+print(a,b,sep="")
+
+
+
+# 6021
+
+a=input()
+
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
