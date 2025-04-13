c2d0d824a15a30088f850f901ce571cf2be79ce9
Leona <102565567+alimhanhan@users.noreply.github.com>
2025-04-13 19:18:52 +0900

    Add files via upload

---
+++ CodeUp_6066.py
@@ -0,0 +1,18 @@
+# 3개의 정수를 입력
+a, b, c = map(int, input().split())
+
+# 각 정수에 대해 짝수/홀수 판단 후 출력
+if a % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+if b % 2 == 0:
+    print("even")
+else:
+    print("odd")
+
+if c % 2 == 0:
+    print("even")
+else:
+    print("odd")
